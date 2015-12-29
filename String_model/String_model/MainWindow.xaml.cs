f0421bd8eef6ca79721a34eb7a263243d2b07298
Владислав Ширинкин <vladislav.shirinkin@outlook.com>
2015-12-30 03:50:54 +0500

    зависимости

--- String_model/String_model/MainWindow.xaml.cs
+++ String_model/String_model/MainWindow.xaml.cs
@@ -24,5 +24,58 @@ namespace String_model
         {
             InitializeComponent();
         }
+
+        private void a_box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (equation_label != null)
+                equation_label.Content = "Utt=(" + a_box.Text + ")^2*Uxx+(" + f_box.Text + ")";
+        }
+
+        private void f_box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (equation_label != null)
+                equation_label.Content = "Utt=(" + a_box.Text + ")^2*Uxx+(" + f_box.Text + ")";
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            left_box.Text = "-∞";
+        }
+
+        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            right_box.Text = "+∞";
+        }
+
+        private void left_box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (first_left_bound_label != null)
+                first_left_bound_label.Content = "U(" + left_box.Text + ",t):";
+            if (two_left_bound_label != null)
+                two_left_bound_label.Content = "Ux(" + left_box.Text + ",t):";
+        }
+
+        private void right_box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (first_right_bound_label != null)
+                first_right_bound_label.Content = "U(" + right_box.Text + ",t):";
+            if (two_right_bound_label != null)
+                two_right_bound_label.Content = "Ux(" + right_box.Text + ",t):";
+        }
+
+        private void OpenGLControl_OpenGLInitialized(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+
+        }
+
+        private void OpenGLControl_Resized(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+
+        }
+
+        private void OpenGLControl_OpenGLDraw(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+
+        }                             
     }
 }
